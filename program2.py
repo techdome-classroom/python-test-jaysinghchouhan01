2e1b40bdc00dc27243a943595b17cd6c7f5aa9e5
jaysinghchouhan01 <jaysinghchouhan0103@gmail.com>
2024-07-26 13:37:05 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,20 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
-        return False
+    i, j = 0, 0
+    while i < len(s) and j < len(p):
+        if p[j] == '*':
+            j += 1
+            while j < len(p) and p[j] == '*':
+                j += 1
+            if j == len(p):
+                return True
+            while i < len(s) and (s[i] != p[j] or p[j] == '?'):
+                i += 1
+        elif p[j] == '?':
+            i += 1
+            j += 1
+        elif s[i] == p[j]:
+            i += 1
+            j += 1
+        else:
+            return False
+    return i == len(s) and j == len(p)
